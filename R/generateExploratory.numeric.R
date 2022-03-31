339d2c062b1e379869b673d411868442ea5c67e7
Tetra <tetra.silv@gmail.com>
2022-03-31 19:09:19 -0400

    Working directory weirdness

--- R/generateExploratory.numeric.R
+++ R/generateExploratory.numeric.R
@@ -8,22 +8,22 @@
 
 generateExploratory.numeric <- function(data, colramp = c("red", "green")) {
 
-	pdf(file = "ExplorationOutput_General.pdf")
+	pdf(file = "~/ExplorationOutput_General.pdf")
 	par(mfrow = c(2, 2))
 	numerics_names <- names(data)[unlist(lapply(data, is.numeric))]
 	for (var in numerics_names) {
   	color <- sample(grDevices::colors()[grep('gr(a|e)y', grDevices::colors(), invert = T)], 1) #Random color for each set of graphs for distinctiveness.
   	hist(data[[var]], col = color, main = paste("Histogram of", var, ""), ylab = "Value", xlab = "Observation # (Sorted)")
   	boxplot(data[var], col = color, main = paste("Boxplot of", var, ""), ylab = "Value", xlab = "Observation # (Sorted)")}
-	dev.off()
+	invisible(dev.off())
 
-	pdf(file = "ExplorationOutput_BinningCheck.pdf")
+	pdf(file = "~/ExplorationOutput_BinningCheck.pdf")
 	par(mfrow = c(1, 1))
 	pal <- colorRampPalette(colramp)
 	for (var in numerics_names) {
 		rampedColors <- pal(length(data[[var]]))
   	plot(1:length(data[[var]]), sort(data[[var]], na.last = TRUE),  main = paste("Binning Check for", var, ""), col = rampedColors, ylab = "Value", xlab = "Observation # (Sorted)")
 	  abline(h = mean(data[[var]], na.rm = TRUE))}
-	dev.off()
+	invisible(dev.off())
 
 }
