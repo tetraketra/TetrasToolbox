99637e73a68fd2b026e4ca883d456dccfa699041
Tetra <tetra.silv@gmail.com>
2022-03-31 18:01:34 -0400

    Assorted useful R and analytics tools for personal use
    I  wouldn't recommend using these yourself.

---
+++ R/safeSource.R
@@ -0,0 +1,20 @@
+#' Safely source a file.
+#'
+#' This function sources a file while installing and/or loading the specified packages,
+#' all in one function. This reduces the need for multiple install.packages or library
+#' calls during impromptu work. No extra error handling is included, so normal package
+#' and source errors will be thrown if applicable.
+#' @param file The file to source.
+#' @param packages The packages that must be installed and/or loaded to safely source the file.
+#' @return Nothing.
+#' @export
+safeSource <- function(file, packages){
+	for (p in packages) {
+		if (p %in% rownames(installed.packages())) {
+    	library(p, character.only = T) } else {
+      install.packages(p, dependencies = T)
+      library(p,character.only = T)}
+     }
+	source(file)
+}
+
